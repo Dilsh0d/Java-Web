f2cd5144c8f3eeb19b71ce5b1929ce999799b818
dilsh0d <dilshod.toj@gmail.com>
2017-10-20 21:44:32 +0500

    Project setup

---
+++ src/uz/java/web/app/servlets/Logouts.java
@@ -0,0 +1,22 @@
+package uz.java.web.app.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by dilsh0d on 07.10.2017.
+ */
+public class Logouts extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().setAttribute("userData",null);
+        response.sendRedirect("/login");
+    }
+}
